d55c9672e946e4d4fce0f7bc7a2557c4697a76f5
devenvoy <iambeast0069@gmail.com>
2026-09-11 10:43:45 +0530

    feat: distribute precompiled release binary in Homebrew Formula

--- Formula/mconv.rb
+++ Formula/mconv.rb
@@ -1,16 +1,30 @@
 class Mconv < Formula
   desc "Interactive high-performance TUI media converter powered by FFmpeg"
   homepage "https://github.com/devenvoy/mconv"
-  url "https://github.com/devenvoy/mconv/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "2d7e501843e2bbd7b39595d39ffc9dedd3c498101d3337dfde707a435c2f9e77"
+  version "2.0.0"
   license "Apache-2.0"
-  head "https://github.com/devenvoy/mconv.git", branch: "main"
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/devenvoy/mconv/releases/download/v2.0.0/mconv-v2.0.0-macos-arm64.tar.gz"
+      sha256 "0280127e41ea22183116440ece96bd60b64d4da5f8bb7b6ea10731bdfb0df3c3"
+    else
+      url "https://github.com/devenvoy/mconv/releases/download/v2.0.0/mconv-v2.0.0-macos-x86_64.tar.gz"
+      sha256 "87191463de508ad5d2891a585a1b9eb3b8a4705cf796e55bef10c700a015a569"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/devenvoy/mconv/releases/download/v2.0.0/mconv-v2.0.0-linux-x86_64.tar.gz"
+      sha256 "eeb6b476742d7dd1580acdedbce067df629422bb9bc6a19392f95ee0038b4329"
+    end
+  end
+
   depends_on "ffmpeg"
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "mconv"
   end
 
   test do
