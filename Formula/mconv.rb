14367bff363f211709ab0916facf3fa320411ee7
devenvoy <iambeast0069@gmail.com>
2026-09-11 09:59:36 +0530

    chore: update release archive sha256 in Formula/mconv.rb

--- Formula/mconv.rb
+++ Formula/mconv.rb
@@ -2,8 +2,7 @@ class Mconv < Formula
   desc "Interactive high-performance TUI media converter powered by FFmpeg"
   homepage "https://github.com/devenvoy/mconv"
   url "https://github.com/devenvoy/mconv/archive/refs/tags/v2.0.0.tar.gz"
-  # Compute sha256 via: curl -sL https://github.com/devenvoy/mconv/archive/refs/tags/v2.0.0.tar.gz | sha256sum
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "4b8c4ba41ba6a2b27be62042e8b85f1d8be348985d0837d73550bc7a43f7ed50"
   license "Apache-2.0"
   head "https://github.com/devenvoy/mconv.git", branch: "main"
 
